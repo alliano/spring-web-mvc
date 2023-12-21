1dc10020fde69a49f2df5b941c2a08b47adaf8a0
alliano <allianoanonymous@gmail.com>
2023-12-21 12:31:25 +0900

    add seting for Http method that allowed

--- src/main/java/com/mvc/springwebmvc/controllers/GreetingController.java
+++ src/main/java/com/mvc/springwebmvc/controllers/GreetingController.java
@@ -3,6 +3,8 @@ package com.mvc.springwebmvc.controllers;
 import java.io.IOException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import com.mvc.springwebmvc.services.GreetingService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,7 +15,8 @@ public class GreetingController {
  
     private final GreetingService greetingService;
 
-    @RequestMapping(path = "/greet")
+    // endpoin ini haya bisa di akses dengan HTTP Method GET
+    @RequestMapping(path = "/greet", method = RequestMethod.GET)
     public void greeting(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.getWriter().println(this.greetingService.greet(request.getParameter("name")));
     }
