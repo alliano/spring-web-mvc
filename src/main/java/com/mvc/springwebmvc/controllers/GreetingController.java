60ab1506b85f2b8d8db02f12480c77667c1cbd87
alliano <allianoanonymous@gmail.com>
2023-12-21 11:54:07 +0900

    add GreetingService

--- src/main/java/com/mvc/springwebmvc/controllers/GreetingController.java
+++ src/main/java/com/mvc/springwebmvc/controllers/GreetingController.java
@@ -1,21 +1,20 @@
 package com.mvc.springwebmvc.controllers;
 
 import java.io.IOException;
-import java.util.Objects;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import com.mvc.springwebmvc.services.GreetingService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
 
-@Controller
+@Controller @AllArgsConstructor
 public class GreetingController {
  
+    private final GreetingService greetingService;
+
     @RequestMapping(path = "/greet")
     public void greeting(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        if(Objects.nonNull(request.getParameter("name"))) response.getWriter().println("Assalamuallaikum ya ".concat(request.getParameter("name")));
-        else
-        response.getWriter().println("Assalmuallikum Brouther..");
+        response.getWriter().println(this.greetingService.greet(request.getParameter("name")));
     }
 }
