34c05e27ea1b44fd3a4b10f172191e24403892fd
alliano <allianoanonymous@gmail.com>
2023-12-25 11:23:31 +0900

    create example for usage of Cookie

--- src/main/java/com/mvc/springwebmvc/controllers/AuthenticationController.java
+++ src/main/java/com/mvc/springwebmvc/controllers/AuthenticationController.java
@@ -3,9 +3,14 @@ package com.mvc.springwebmvc.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller @RequestMapping(path = "/auth")
 public class AuthenticationController {
@@ -17,4 +22,24 @@ public class AuthenticationController {
         else
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized!");
     }
+
+    @PostMapping(path = "/addCookie")
+    public ResponseEntity<?> setCookie(@RequestParam(value = "username") String username, @RequestParam(value = "password") String password, HttpServletResponse response) {
+        if (username.equals("test") && password.equals("pass")) {
+            Cookie cookie = new Cookie("username", username);
+            cookie.setPath("/");
+            response.addCookie(cookie);
+            return ResponseEntity.status(HttpStatus.OK).body("Success Authenticated!");
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized!");
+        }
+    }
+
+    @GetMapping(path = "/user")
+    public ResponseEntity<?> getCookie(@CookieValue(name = "username") String username) {
+        return ResponseEntity.status(HttpStatus.OK).body(username);
+    }
 }
+
+
--- src/test/java/com/mvc/springwebmvc/ApplicationTest.java
+++ src/test/java/com/mvc/springwebmvc/ApplicationTest.java
@@ -1,15 +1,18 @@
 package com.mvc.springwebmvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import jakarta.servlet.http.Cookie;
 
 @SpringBootTest(classes = SpringWebMvcApplication.class) @AutoConfigureMockMvc
 public class ApplicationTest {
@@ -39,4 +42,27 @@ public class ApplicationTest {
             content().string(Matchers.containsString("Unauthorized!"))
         );
     }
+
+    @Test
+    public void testCookie() throws Exception {
+        this.mockMvc.perform(
+            post("/auth/addCookie")
+            .queryParam("username", "test")
+            .queryParam("password", "pass")
+        ).andExpectAll(
+            status().isOk(),
+            content().string(Matchers.containsString("Success Authenticated!")),
+            cookie().value("username", "test")
+
+        );
+
+        this.mockMvc.perform(
+            get("/auth/user")
+            .cookie(new Cookie("username", "test"))
+        ).andExpectAll(
+            status().isOk(),
+            content().string(Matchers.containsString("test"))
+        );
+    }
+    
 }
