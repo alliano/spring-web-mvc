29bdd89187217d01ca0f4ea12444730cdcf1a7a4
alliano <allianoanonymous@gmail.com>
2023-12-29 17:51:22 +0900

    deleting some line

--- src/main/java/com/mvc/springwebmvc/controllers/HomeController.java
+++ src/main/java/com/mvc/springwebmvc/controllers/HomeController.java
@@ -4,7 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 
