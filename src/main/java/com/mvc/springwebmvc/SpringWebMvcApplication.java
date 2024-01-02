72ab2b32f6ea0e03f1c6464f20ea7a4a415b21d9
alliano <allianoanonymous@gmail.com>
2024-01-02 17:42:10 +0900

    enable FeignClient

--- src/main/java/com/mvc/springwebmvc/SpringWebMvcApplication.java
+++ src/main/java/com/mvc/springwebmvc/SpringWebMvcApplication.java
@@ -2,12 +2,11 @@ package com.mvc.springwebmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication @EnableFeignClients
 public class SpringWebMvcApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebMvcApplication.class, args);
 	}
-
 }
