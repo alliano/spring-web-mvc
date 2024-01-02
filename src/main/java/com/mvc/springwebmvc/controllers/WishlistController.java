1509ad1bfaa7d2aacc9180bdef5ed8ab5a0c1fd0
alliano <allianoanonymous@gmail.com>
2024-01-02 17:47:18 +0900

    create getTodo

--- src/main/java/com/mvc/springwebmvc/controllers/WishlistController.java
+++ src/main/java/com/mvc/springwebmvc/controllers/WishlistController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ public class WishlistController {
     private final List<String> wishlist = new ArrayList<String>();
 
     @GetMapping(path = "/wishlist")
-    public List<String> addTodo(@RequestParam(value = "wishlist", required = true) String wishlist) {
+    public List<String> addWishlist(@RequestParam(value = "wishlist", required = true) String wishlist) {
         this.wishlist.add(wishlist);
         return List.of(wishlist);
     }
@@ -21,5 +22,10 @@ public class WishlistController {
     @GetMapping(path = "/wishlists")
     public List<String> getAllTodo() {
         return this.wishlist;
+    }
+
+    @PostMapping(path = "/testRest")
+    public List<String> get(@RequestParam(value = "name") String name) {
+        return List.of(name);
     }
 }
