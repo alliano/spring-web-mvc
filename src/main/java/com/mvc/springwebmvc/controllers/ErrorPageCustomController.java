a8ea79778543d15bf08e37203d28a4722d6223de
alliano <allianoanonymous@gmail.com>
2024-01-04 08:40:16 +0900

    create custom page error

--- src/main/java/com/mvc/springwebmvc/controllers/ErrorPageCustomController.java
+++ src/main/java/com/mvc/springwebmvc/controllers/ErrorPageCustomController.java
@@ -1,9 +1,9 @@
 package com.mvc.springwebmvc.controllers;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.ResponseEntity;
@@ -13,17 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.SneakyThrows;
 
 @Controller
 public class ErrorPageCustomController implements ErrorController {
     
-    @RequestMapping(path = "/error")
-    public ResponseEntity<?> error(HttpServletRequest request) throws IOException {
-        Integer statusCode = (Integer)request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+    @RequestMapping(path = "/error") @SneakyThrows
+    public ResponseEntity<?> error(HttpServletRequest request) {
+        Optional<Object> statusCode = Optional.ofNullable(request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE));
         String errorMessage = (String)request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
         File file = ResourceUtils.getFile("classpath:pages/errorPage.html");
         String raw = new String(Files.readAllBytes(Path.of(file.getPath())));
-        String html = raw.replace("$StatusCode", statusCode.toString()).replace("$Message", errorMessage);
-        return ResponseEntity.status(statusCode).body(html);
+        String html = raw.replace("$StatusCode", statusCode.get().toString()).replace("$Message", errorMessage);
+        return ResponseEntity.status((Integer)statusCode.get()).body(html);
     }
 }
